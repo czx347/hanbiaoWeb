c4c82616bc6f91622cda6df264a533ff7f132783
347 <948773471@qq.com>
2021-04-21 17:45:47 +0800

    ''

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Table/>
+  </div>
+</template>
+
+<script>
+import Table from './components/table.vue'
+
+export default {
+  name: 'app',
+  components: {
+      Table,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+
+}
+</style>
---
+++ src/components/table.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-calendar v-model="value">
+    </el-calendar>
+</template>
+
+<script>
+    export default {
+        name: "",
+        data() {
+            return {
+                value: new Date()
+            };
+        },
+        methods: {
+            handleClose(done) {
+                this.$confirm('确认关闭？')
+                    .then(_ => {
+                        done();
+                    })
+                    .catch(_ => {});
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
